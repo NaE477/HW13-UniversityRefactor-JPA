587acb6a36849b28634b99f43ccadb67d200df26
NaeRae <naeimr4@gmail.com>
2022-03-08 16:43:44 +0330

    Student and term Repository refactored to EntityManager and EntityManagerFactory

--- src/main/java/repos/StudentRep.java
+++ src/main/java/repos/StudentRep.java
@@ -4,66 +4,63 @@ import models.things.Course;
 import models.users.Student;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 public class StudentRep extends BaseRepository<Student> {
-    public StudentRep(SessionFactory sessionFactory) {
-        super(sessionFactory);
+    public StudentRep(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
     }
 
     public Student read(Integer id) {
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                return session.get(Student.class, id);
-            } catch (Exception e) {
-                return null;
-            }
+        var session = entityManagerFactory.createEntityManager();
+        try {
+            return session.find(Student.class, id);
+        } catch (Exception e) {
+            return null;
         }
     }
 
     public Student read(String username) {
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-                var criteriaQuery = criteriaBuilder.createQuery(Student.class);
-                var root = criteriaQuery.from(Student.class);
-                var query = criteriaQuery
-                        .select(root)
-                        .where(criteriaBuilder.equal(root.get("username"),username));
-                return session.createQuery(query).getSingleResult();
-            } catch (Exception e) {
-                return null;
-            }
+        var session = entityManagerFactory.createEntityManager();
+        try {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            var criteriaQuery = criteriaBuilder.createQuery(Student.class);
+            var root = criteriaQuery.from(Student.class);
+            var query = criteriaQuery
+                    .select(root)
+                    .where(criteriaBuilder.equal(root.get("username"), username));
+            return session.createQuery(query).getSingleResult();
+        } catch (Exception e) {
+            return null;
         }
     }
 
     public List<Student> readAll() {
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-                var criteriaQuery = criteriaBuilder.createQuery(Student.class);
-                var root = criteriaQuery.from(Student.class);
-                var query = criteriaQuery
-                        .select(root);
-                return session.createQuery(query).list();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
+        var session = entityManagerFactory.createEntityManager();
+        try {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            var criteriaQuery = criteriaBuilder.createQuery(Student.class);
+            var root = criteriaQuery.from(Student.class);
+            var query = criteriaQuery
+                    .select(root);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
-    public List<Student> readAll(Course course){
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                return session
-                        .createQuery("select s from Student s left join fetch s.grades g where g.course.id = :courseId",Student.class)
-                        .setParameter("courseId",course.getId())
-                        .list();
-            } catch (Exception e) {
-                return null;
-            }
+    public List<Student> readAll(Course course) {
+        var session = entityManagerFactory.createEntityManager();
+        try {
+            return session
+                    .createQuery("select s from Student s left join fetch s.grades g where g.course.id = :courseId", Student.class)
+                    .setParameter("courseId", course.getId())
+                    .getResultList();
+        } catch (Exception e) {
+            return null;
         }
     }
 }
--- src/main/java/repos/TermRepository.java
+++ src/main/java/repos/TermRepository.java
@@ -3,38 +3,40 @@ package repos;
 import models.things.Term;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 public class TermRepository extends BaseRepository<Term> {
 
-    public TermRepository(SessionFactory sessionFactory) {
-        super(sessionFactory);
+    public TermRepository(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
     }
 
     public Term read() {
-        try (var session = entityManagerFactory.openSession()) {
-            return session.createQuery("select t from Term t where t.term = (select max(term) from t)", Term.class).getSingleResult();
-        }
+        var session = entityManagerFactory.createEntityManager();
+        return session.createQuery("select t from Term t where t.term = (select max(term) from t)", Term.class).getSingleResult();
     }
+
     public Term readFirst() {
-        try (var session = entityManagerFactory.openSession()) {
-            return session.createQuery("select t from Term t where t.term = (select min(term) from t)", Term.class).getSingleResult();
-        }
+        var session = entityManagerFactory.createEntityManager();
+        return session.createQuery("select t from Term t where t.term = (select min(term) from t)", Term.class).getSingleResult();
     }
+
     public List<Term> readAll() {
-        try (var session = entityManagerFactory.openSession()) {
-            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-            var criteriaQuery = criteriaBuilder.createQuery(Term.class);
-            var root = criteriaQuery.from(Term.class);
-            var query = criteriaQuery.select(root);
-            return session.createQuery(criteriaQuery).list();
-        }
+        var session = entityManagerFactory.createEntityManager();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        var criteriaQuery = criteriaBuilder.createQuery(Term.class);
+        var root = criteriaQuery.from(Term.class);
+        var query = criteriaQuery.select(root);
+        return session.createQuery(query).getResultList();
     }
 
     @Override
-    public void update(Term term) {}
+    public void update(Term term) {
+    }
 
     @Override
-    public void delete(Term term) {}
+    public void delete(Term term) {
+    }
 }
