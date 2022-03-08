861eb7cb3845c2d0e4ce07839fb72c57c35a4f97
NaeRae <naeimr4@gmail.com>
2022-03-08 16:34:22 +0330

    Clerk Repository refactored to EntityManager and EntityManagerFactory

--- src/main/java/repos/ClerkRep.java
+++ src/main/java/repos/ClerkRep.java
@@ -1,56 +1,51 @@
 package repos;
 
 import models.users.Clerk;
-import org.hibernate.SessionFactory;
 
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 public class ClerkRep extends BaseRepository<Clerk> {
-    public ClerkRep(SessionFactory sessionFactory) {
-        super(sessionFactory);
+    public ClerkRep(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
     }
 
     public Clerk read(Integer id) {
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                return session.get(Clerk.class, id);
-            } catch (Exception e) {
-                return null;
-            }
+        var entityManager = entityManagerFactory.createEntityManager();
+        try {
+            return entityManager.find(Clerk.class, id);
+        } catch (Exception e) {
+            return null;
         }
     }
 
     public Clerk read(String username) {
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                return session
-                        .createQuery("from Clerk where username = :username",Clerk.class)
-                        .setParameter("username",username)
-                        .getSingleResult();
-            } catch (Exception e) {
-                return null;
-            }
+        var entityManager = entityManagerFactory.createEntityManager();
+        try {
+            return entityManager
+                    .createQuery("from Clerk where username = :username", Clerk.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
         }
     }
 
     public List<Clerk> readAll() {
-        try (var session = entityManagerFactory.openSession()) {
-            var transaction = session.beginTransaction();
-            try {
-                /*return session.createQuery("select c from Clerk c",Clerk.class).list();*/
-                CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-                var criteriaQuery = criteriaBuilder.createQuery(Clerk.class);
-                var root = criteriaQuery.from(Clerk.class);
-                var query = criteriaQuery
-                        .select(root);
-                return session.createQuery(query).list();
-            } catch (Exception e) {
-                e.printStackTrace();
-                transaction.rollback();
-                System.out.println("Database changes rolled back due to error.");
-                throw e;
-            }
+        var entityManager = entityManagerFactory.createEntityManager();
+        try {
+            /*return entityManager.createQuery("select c from Clerk c",Clerk.class).list();*/
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            var criteriaQuery = criteriaBuilder.createQuery(Clerk.class);
+            var root = criteriaQuery.from(Clerk.class);
+            var query = criteriaQuery
+                    .select(root);
+            return entityManager.createQuery(query).getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Database changes rolled back due to error.");
+            throw e;
         }
     }
 }
