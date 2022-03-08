ab91cea64b984d05568abb093821d201bef2fecc
NaeRae <naeimr4@gmail.com>
2022-03-08 16:39:15 +0330

    Grade Repository refactored to EntityManager and EntityManagerFactory

--- src/main/java/repos/GradeRep.java
+++ src/main/java/repos/GradeRep.java
@@ -5,38 +5,37 @@ import models.things.Grade;
 import models.users.Student;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
 public class GradeRep extends BaseRepository<Grade> {
 
-    public GradeRep(SessionFactory sessionFactory) {
-        super(sessionFactory);
+    public GradeRep(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
     }
 
     public List<Grade> readAllByStudent(Student student) {
-        try(var session = entityManagerFactory.openSession()) {
-            try {
-                return session
-                        .createQuery("select g from Grade g where g.student.id = :sId", Grade.class)
-                        .setParameter("sId", student.getId())
-                        .list();
-            } catch (Exception e) {
-                return null;
-            }
+        var entityManager = entityManagerFactory.createEntityManager();
+        try {
+            return entityManager
+                    .createQuery("select g from Grade g where g.student.id = :sId", Grade.class)
+                    .setParameter("sId", student.getId())
+                    .getResultList();
+        } catch (Exception e) {
+            return null;
         }
     }
 
     public Grade read(Student student, Course course) {
-        try(var session = entityManagerFactory.openSession()) {
-            try {
-                return session
-                        .createQuery("select g from Grade g where g.course.id = :cId and g.student.id = :sId", Grade.class)
-                        .setParameter("cId", course.getId())
-                        .setParameter("sId", student.getId())
-                        .getSingleResult();
-            } catch (Exception e) {
-                return null;
-            }
+        var entityManager = entityManagerFactory.createEntityManager();
+        try {
+            return entityManager
+                    .createQuery("select g from Grade g where g.course.id = :cId and g.student.id = :sId", Grade.class)
+                    .setParameter("cId", course.getId())
+                    .setParameter("sId", student.getId())
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
         }
     }
 }
