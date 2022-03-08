52bedb50cb05d901e744e91b8bf930b177bd9235
NaeRae <naeimr4@gmail.com>
2022-03-08 16:40:46 +0330

    Professor Repository refactored to EntityManager and EntityManagerFactory

--- src/main/java/repos/ProfessorRep.java
+++ src/main/java/repos/ProfessorRep.java
@@ -3,52 +3,51 @@ package repos;
 import models.users.Professor;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 public class ProfessorRep extends BaseRepository<Professor> {
-    public ProfessorRep(SessionFactory sessionFactory) {
-        super(sessionFactory);
+    public ProfessorRep(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
     }
 
     public Professor read(Integer id) {
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                return session.get(Professor.class, id);
-            } catch (Exception e) {
-                return null;
-            }
+        var session = entityManagerFactory.createEntityManager();
+        try {
+            return session.find(Professor.class, id);
+        } catch (Exception e) {
+            return null;
         }
     }
 
-    public Professor read(String username){
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-                var criteriaQuery = criteriaBuilder.createQuery(Professor.class);
-                var root = criteriaQuery.from(Professor.class);
-                var query = criteriaQuery
-                        .select(root)
-                        .where(criteriaBuilder.equal(root.get("username"),username));
-                return session.createQuery(query).getSingleResult();
-            } catch (Exception e) {
-                return null;
-            }
+    public Professor read(String username) {
+        var session = entityManagerFactory.createEntityManager();
+        try {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            var criteriaQuery = criteriaBuilder.createQuery(Professor.class);
+            var root = criteriaQuery.from(Professor.class);
+            var query = criteriaQuery
+                    .select(root)
+                    .where(criteriaBuilder.equal(root.get("username"), username));
+            return session.createQuery(query).getSingleResult();
+        } catch (Exception e) {
+            return null;
         }
     }
-    public List<Professor> readAll(){
-        try (var session = entityManagerFactory.openSession()) {
-            try {
-                CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-                var criteriaQuery = criteriaBuilder.createQuery(Professor.class);
-                var root = criteriaQuery.from(Professor.class);
-                var query = criteriaQuery
-                        .select(root);
-                return session.createQuery(query).list();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
+
+    public List<Professor> readAll() {
+        var session = entityManagerFactory.createEntityManager();
+        try {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            var criteriaQuery = criteriaBuilder.createQuery(Professor.class);
+            var root = criteriaQuery.from(Professor.class);
+            var query = criteriaQuery
+                    .select(root);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
